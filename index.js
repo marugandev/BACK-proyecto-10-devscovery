68632484b97f7eb0d582650cfa2db8af483c0ceb
marugandev <marugandev@gmail.com>
2024-12-11 14:22:37 +0100

    UPDATE PORT index for deploy 2

--- index.js
+++ index.js
@@ -21,5 +21,5 @@ app.use("*", (req, res, next) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`"Server is running on port ${PORT}"`);
+  console.log(`"Server is running on port: ${PORT}"`);
 });
