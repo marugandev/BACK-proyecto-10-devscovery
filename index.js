41563fc885b70659fe167107de0796ef4dfb2b23
marugandev <marugandev@gmail.com>
2025-01-09 13:12:33 +0100

    FIX limit upload images

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ const mainRouter = require("./src/api/routes/mainRouter");
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(express.json());
+/* app.use(express.json()); */
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
 app.use(cors());
 
 connectDB();
